05d1639705b8552a3eb57007350e833d0c31ec32
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-25 12:33:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -263,7 +263,7 @@ client.on("message", message => {
 
  
  
-  client.on('message', message => {
+ client.on('message', message => {
    if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
